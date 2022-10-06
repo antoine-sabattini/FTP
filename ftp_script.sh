5cf9ac30b8848e8d5623c8eff57ebe4cb1cf4b11
antoine.sabattini <antoine.sabattini@laplateforme.io>
2022-10-06 19:22:14 +0200

    scripting

---
+++ ftp_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Installation de proftpd/openssl
+sudo apt install sudo adduser openssl proftpd-*
+#création du dossier ssl
+sudo mkdir /etc/proftpd/ssl
+#générations Tls/ssl
+sudo openssl req -new -x509 -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem -keyout /etc/proftpd/ssl/proftpd.key.pem
+#copie des fichiers de configurations modifiés
+sudo cp /files_proftpd/proftpd.conf /etc/proftpd/proftpd.conf
+sudo cp /files_proftpd/tls.conf /etc/proftpd/tls.conf
+sudo cp /files_proftpd/modules.conf /etc/proftpd/modules.conf
+sudo chmod -R 750 /etc/proftpd/ssl
+#relancement du service
+sudo service proftpd restart
