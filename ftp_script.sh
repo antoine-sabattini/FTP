b3e3cb159c936970621893429b1235c26507f0ab
antoine.sabattini <antoine.sabattini@laplateforme.io>
2022-10-07 10:43:29 +0200

    script2

--- ftp_script.sh
+++ ftp_script.sh
@@ -7,9 +7,9 @@ sudo mkdir /etc/proftpd/ssl
 #générations Tls/ssl
 sudo openssl req -new -x509 -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem -keyout /etc/proftpd/ssl/proftpd.key.pem
 #copie des fichiers de configurations modifiés
-sudo cp /files_proftpd/proftpd.conf /etc/proftpd/proftpd.conf
-sudo cp /files_proftpd/tls.conf /etc/proftpd/tls.conf
-sudo cp /files_proftpd/modules.conf /etc/proftpd/modules.conf
+sudo cp proftpd.conf /etc/proftpd/proftpd.conf
+sudo cp tls.conf /etc/proftpd/tls.conf
+sudo cp modules.conf /etc/proftpd/modules.conf
 sudo chmod -R 750 /etc/proftpd/ssl
 #relancement du service
 sudo service proftpd restart
